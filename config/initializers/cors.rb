3b80773a8e196330836b5ae04758246315edd78f
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2021-03-31 21:11:04 -0400

    add netlify to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://financialmodelingprep.com'
+    origins 'localhost:8080', 'https://priceless-kare-0ae047.netlify.app/', ''
 
     resource '*',
       headers: :any,
