afbb90c74ff181da9f3752ea10c2cd3abe603cc6
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2024-01-15 20:10:19 -0700

    fixed a typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,14 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-allow do
-  origins 'localhost:8080', 
-          'https://diamondhandss.netlify.app',
-          'https://main--diamondhandss.netlify.app', 
-          'https://financialmodelingprep.com'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:8080', 
+            'https://diamondhandss.netlify.app',
+            'https://main--diamondhandss.netlify.app', # Add this line
+            'https://financialmodelingprep.com'
 
-  resource '*',
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head]
-end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
